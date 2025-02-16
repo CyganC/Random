e2a734f6237bbf7e158c1f1bb7ada33cf1f04f63
Your Name <you@example.com>
2025-02-16 14:58:58 +0100

    Add project files

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace ConsoleApp8
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var random = new Random();
+            int randomNamber=random.Next(0,100);
+
+         
+
+            var liczba = 0;
+            
+            while (true)
+            {  
+                var namber = int.Parse(Console.ReadLine());
+               
+                liczba++;
+                if (randomNamber > namber) 
+                {
+                    Console.WriteLine("Podaj większą liczbę");
+                    continue;
+                }
+                
+                if (randomNamber < namber)
+                {
+                    Console.WriteLine("Podaj mniejsząszą liczbę");
+                    continue;
+                }
+                Console.WriteLine("super. wykonano:"+liczba+"prób");
+                break;
+            }
+            
+        }
+
+    //    private static int GetNumber()
+    //    {
+    //        int number =int.Parse(Console.ReadLine());
+    //        return number;
+
+    //    }
+    }
+}
